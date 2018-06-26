8e1d28c60cb6eca33bbd627d610134176995622a
Laurentiu Ichim <lichim@esriro.ro>
2018-06-26 11:46:37 +0300

    Commit 2

--- PopupArcade/PopupArcade/app.js
+++ PopupArcade/PopupArcade/app.js
@@ -1,17 +1,35 @@
-"use strict";
 /// <reference path="@types/arcgis-js-api/index.d.ts" />
-Object.defineProperty(exports, "__esModule", { value: true });
-var Esriro;
-(function (Esriro) {
-    var ViewModel;
-    (function (ViewModel_1) {
-        var ViewModel = /** @class */ (function () {
-            function ViewModel(settings) {
-                this.settings = settings;
-            }
-            return ViewModel;
-        }());
-        ViewModel_1.ViewModel = ViewModel;
-    })(ViewModel = Esriro.ViewModel || (Esriro.ViewModel = {}));
-})(Esriro || (Esriro = {}));
+define(["require", "exports", "esri/Map", "esri/views/MapView"], function (require, exports, Map, MapView) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Esriro;
+    (function (Esriro) {
+        var ViewModel;
+        (function (ViewModel_1) {
+            var ViewModel = /** @class */ (function () {
+                function ViewModel(settings) {
+                    this.settings = settings;
+                    this.map = undefined;
+                    this.mapView = undefined;
+                }
+                ViewModel.prototype.wrap = function () {
+                    this.map = new Map({
+                        basemap: "streets"
+                    });
+                    this.mapView = new MapView({
+                        map: this.map,
+                        center: [26.252, 46.452],
+                        zoom: 10,
+                        container: "viewDiv"
+                    });
+                };
+                return ViewModel;
+            }());
+            ViewModel_1.ViewModel = ViewModel;
+        })(ViewModel = Esriro.ViewModel || (Esriro.ViewModel = {}));
+    })(Esriro || (Esriro = {}));
+    var view_model = Esriro.ViewModel;
+    var view = new view_model.ViewModel(null);
+    view.wrap();
+});
 //# sourceMappingURL=app.js.map
--- PopupArcade/PopupArcade/app.ts
+++ PopupArcade/PopupArcade/app.ts
@@ -5,36 +5,50 @@ import MapView = require("esri/views/MapView");
 
 
 module Esriro.ViewModel
-{
+{/*Permite afisarea componentelor principale ale aplicatiei (Map/ViewMap) si adaugarea de surse de date (FeatureLayers)*/
+
     export interface IViewModel
     {
-
+        wrap(): void;
     }
-
     export interface IViewModelSettings
     {
         mapSettings: IMapSettings;
         mapViewSettings: IMapViewSettings;
     }
-
-    export interface IMapSettings
-    {
+    export interface IMapSettings {
         basemap: string;
     }
-
     export interface IMapViewSettings
     {
         center: number[];
         zoom: number;
         divId: string;
     }
-
     export class ViewModel implements IViewModel
-    {
+    {/*Permite configurarea Map si MapView*/
+        map = undefined;
+        mapView = undefined;
         constructor(public settings: IViewModelSettings)
-        {
+        {}
+        wrap(): void {
+            this.map = new Map({
+                basemap:"streets"
+            });
+            this.mapView = new MapView({
+                map: this.map,
+                center: [26.252, 46.452],
+                zoom: 10,
+                container:"viewDiv"
+            });
 
         }
+
     }
 }
+
+import view_model = Esriro.ViewModel;
+let view: view_model.IViewModel = new view_model.ViewModel(null);
+view.wrap();
+
 
