4a796d0d7d49ca55a719c2850cca25b624cb8f4e
Laurentiu Ichim <lichim@esriro.ro>
2018-06-26 11:28:08 +0300

    commit 1

--- PopupArcade/PopupArcade/app.js
+++ PopupArcade/PopupArcade/app.js
@@ -1,4 +1,17 @@
 "use strict";
 /// <reference path="@types/arcgis-js-api/index.d.ts" />
 Object.defineProperty(exports, "__esModule", { value: true });
+var Esriro;
+(function (Esriro) {
+    var ViewModel;
+    (function (ViewModel_1) {
+        var ViewModel = /** @class */ (function () {
+            function ViewModel(settings) {
+                this.settings = settings;
+            }
+            return ViewModel;
+        }());
+        ViewModel_1.ViewModel = ViewModel;
+    })(ViewModel = Esriro.ViewModel || (Esriro.ViewModel = {}));
+})(Esriro || (Esriro = {}));
 //# sourceMappingURL=app.js.map
--- PopupArcade/PopupArcade/app.ts
+++ PopupArcade/PopupArcade/app.ts
@@ -1,5 +1,40 @@
 ﻿/// <reference path="@types/arcgis-js-api/index.d.ts" />
 
-
 import Map = require("esri/Map");
+import MapView = require("esri/views/MapView");
+
+
+module Esriro.ViewModel
+{
+    export interface IViewModel
+    {
+
+    }
+
+    export interface IViewModelSettings
+    {
+        mapSettings: IMapSettings;
+        mapViewSettings: IMapViewSettings;
+    }
+
+    export interface IMapSettings
+    {
+        basemap: string;
+    }
+
+    export interface IMapViewSettings
+    {
+        center: number[];
+        zoom: number;
+        divId: string;
+    }
+
+    export class ViewModel implements IViewModel
+    {
+        constructor(public settings: IViewModelSettings)
+        {
+
+        }
+    }
+}
 
